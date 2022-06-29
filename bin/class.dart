ec5506b3ee6230604c47553b98b2e06904614fd5
dysandi <an.sandi15@gmail.com>
2022-06-29 20:21:00 +0700

    modified

--- bin/class.dart
+++ bin/class.dart
@@ -1,7 +1,7 @@
 class Person {}
 
 void main() {
-  var person1 = Person(); // instance object
+  var person1 = Person(); // instance object Person
   Person person2 = Person();
 
   print(person1);
